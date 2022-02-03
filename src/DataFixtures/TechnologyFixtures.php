525f176e4061bcae56037958629ec374454227fe
weisgerber <akumaony>
2022-02-03 20:15:35 +0100

    repair

---
+++ src/DataFixtures/TechnologyFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Technology;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TechnologyFixtures extends Fixture
+{
+    const TECHNOLOGIES = ['HTML', 'CSS', 'PHP', 'Symfony', 'Twig', 'Git', 'Figma', 'MySQL',' Photoshop', 'Inkscape','Illustrator', 'Autocad','3DSMax', 'InDesign', 'Illutrator', 'Premiere', 'Final Cut Pro', 'Bootstrap', 'SCRUM', 'JavaScript'];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::TECHNOLOGIES as $key => $technology) {
+            $newTechnology = new Technology();
+
+            $newTechnology->setName($technology);
+
+            $manager->persist($newTechnology);
+            $this->addReference($technology, $newTechnology);
+        }
+
+        $manager->flush();
+    }
+}
