5170ebaaa6f3e4ee63272737ad47db44e4b21a23
weisgerber <akumaony>
2022-02-03 09:36:46 +0100

    edit project

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,22 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('link')
-            ->add('startDate')
-            ->add('endDate')
+            ->add('title', TextType::class, ['label' =>'Titre'])
+            ->add('description', TextareaType::class, ['label' =>'Description'])
+            ->add('link', UrlType::class, [
+                'label' => 'Site internet',
+                'required' => false
+            ])
+            ->add('startDate', DateType::class, [
+                'label' => 'Date de début',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('endDate', DateType::class, [
+                'label' => 'Date de fin',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
         ;
     }
 
